1e22156c6ee3e272aab59d082a96c23481945960
pkumar-code <pkumar.c028@gmail.com>
2025-03-26 07:12:59 +0530

    Update -3  MyBootApplication.java

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -11,6 +11,7 @@ System.out.println("Update -1 Srinivas");
   System.out.println("Update -2 by Srinivas");
    System.out.println("Update -3 by Kumar ");
     System.out.println("Update -4 by Kumar ");
+   System.out.println("Update -5 by Kumar ");
 
 SpringApplication.run(MyBootApplication.class, args);
 }
